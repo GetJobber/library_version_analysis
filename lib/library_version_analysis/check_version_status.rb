0f210d326eeba056b9a1d01d17e6888251055ff7
John Zittlau <john@jazsoft.ca>
2023-08-23 12:50:56 -0600

    Add test slack call temporarily

--- lib/library_version_analysis/check_version_status.rb
+++ lib/library_version_analysis/check_version_status.rb
@@ -179,6 +179,8 @@ module LibraryVersionAnalysis
     def notify(results)
       recent_time = Time.now - 25 * 60 * 60
 
+      SlackNotify.notify("Don't panic. Just testing, to make slack alerts from lib analysis still happen", "security-alerts")
+
       results.each do |hash_line|
         line = hash_line[1]
         if (!line.dependabot_created_at.nil? && line.dependabot_created_at > recent_time )
