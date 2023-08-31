122e88fd8b8260a00fa1ccdc693db708104bd6b9
John Zittlau <john@jazsoft.ca>
2023-08-31 09:27:08 -0600

    Temp putting back ugly OpenSSL hack

--- lib/library_version_analysis/check_version_status.rb
+++ lib/library_version_analysis/check_version_status.rb
@@ -4,6 +4,9 @@ require "open3"
 require "pry"
 
 module LibraryVersionAnalysis
+  # TODO: This is evil and must be removed when I return
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+  
   Versionline = Struct.new(
     :owner,
     :parent,
