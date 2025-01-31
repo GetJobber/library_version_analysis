634d44ba24fe06bfd0c9d0f4a228e10e56f0c5e9
John Zittlau <john.z@getjobber.com>
2025-01-30 22:14:01 -0700

    Set server upload logging to be guarded by DEV_OUTPUT

--- lib/library_version_analysis/library_tracking.rb
+++ lib/library_version_analysis/library_tracking.rb
@@ -4,7 +4,7 @@ module LibraryVersionAnalysis
   class LibraryTracking
     def self.upload(json_data)
       uri = URI(ENV["LIBRARY_UPLOAD_URL"])
-      puts "  updating server at #{uri}"
+      puts "  updating server at #{uri}" if LibraryVersionAnalysis::DEV_OUTPUT
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = uri.scheme == "https"
       http.read_timeout = 300
@@ -12,7 +12,7 @@ module LibraryVersionAnalysis
       req["X-Upload-Key"] = ENV["UPLOAD_KEY"]
       req.body = json_data
       res = http.request(req)
-      puts "response #{res.code}:#{res.msg}\n#{res.body}"
+      puts "response #{res.code}:#{res.msg}\n#{res.body}" if LibraryVersionAnalysis::DEV_OUTPUT
     end
   end
 end
